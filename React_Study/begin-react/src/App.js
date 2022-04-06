0ef373dc1937023527d993bce8aa38c35a9ba69c
minerva421 <minerva421.ai@gmail.com>
2022-04-06 12:55:09 +0900

    props를 통해 컴포넌트에게 값 전달하기

--- React_Study/begin-react/src/App.js
+++ React_Study/begin-react/src/App.js
@@ -1,6 +1,8 @@
 // import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import Hello from "./Hello";
+import Wrapper from './Wrapper';
 
 function App() {
   const name = "react";
@@ -12,9 +14,12 @@ function App() {
   }
   return (
     <>
-      <Hello />
-      <div style={style}>{name}</div> 
-      <div className="gray-box"></div>
+    <Wrapper>
+      <Hello name="react" color="green" fontSize="20px"/>
+        <Hello color="pink" fontSize="20px"/>
+        <div style={style}>{name}</div> 
+        <div className="gray-box"></div>
+    </Wrapper>
     </>
   );
 }
--- React_Study/begin-react/src/Hello.js
+++ React_Study/begin-react/src/Hello.js
@@ -1,7 +1,15 @@
 import React from "react";
 
-function Hello() {
-    return <div>Hello <button>Button</button> </div>
+function Hello({color, fontSize, name}) {
+    return (
+        <div style={{color : color, fontSize : fontSize}}>
+            Hello {name} 
+        </div>
+    )
+}
+
+Hello.defaultProps = {
+    name : 'Nobody'
 }
 
 export default Hello;
